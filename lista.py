85a9281dd276ba8ac945c78e3848d00abc4075b6
unknown <lopesdeandradejoaovictor@gmail.com>
2023-08-18 21:54:12 -0300

    lista

---
+++ lista.py
@@ -0,0 +1,16 @@
+lista1 = ['diemes','nunes','souza','caio','luana','amanda']
+print (type (lista1))
+print (len (lista1))
+print (lista1[4])
+
+lista2 = lista1 *2
+print(lista1)
+
+lista1.append("Amanda")
+print(lista1)
+
+lista1. remove("caio")
+print(lista1)
+
+lista1.pop(3)
+print(lista1)
